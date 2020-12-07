5e14235ecb7c29ece7e8506f035b039793c7274e
adou <321155060@qq.com>
2020-12-07 16:26:47 +0800

    add index and bootstrap

--- src/main/java/adou/community/controller/HelloController.java
+++ src/main/java/adou/community/controller/HelloController.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public String hello(){
+        return "index";
     }
 }
