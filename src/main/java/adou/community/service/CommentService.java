1353c226a1737b16a705f949a18b097242443cd1
adou <321155060@qq.com>
2021-01-19 09:42:15 +0800

    给回复功能添加事务

--- src/main/java/adou/community/service/CommentService.java
+++ src/main/java/adou/community/service/CommentService.java
@@ -10,6 +10,7 @@ import adou.community.model.Comment;
 import adou.community.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
@@ -23,6 +24,7 @@ public class CommentService {
     @Resource
     private QuestionExtMapper questionExtMapper;
 
+    @Transactional
     public void insert(Comment comment) {
         if (comment.getParentId() == null || comment.getParentId() == 0) {
             throw new CustomizeException(CustomizeErrorCode.TARGET_PARAM_NOT_FOUND);
